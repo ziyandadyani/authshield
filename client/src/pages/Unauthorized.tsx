0305029592f0e69743abe5338a163e05cfea7446
Ziyanda Dyani <ziyandadyani17@gmail.com>
2026-04-27 16:57:32 +0200

    update : styles-forbidden page

--- client/src/pages/Unauthorized.tsx
+++ client/src/pages/Unauthorized.tsx
@@ -1,6 +1,6 @@
 const Unauthorized = () => {
   return (
-    <div className="centered">
+    <div className="center">
       <h1>403 : Forbidden</h1>
       <h3>Sorry , you are not authorized to view this page</h3>
     </div>
